db615de1656fc133b854fe9cc974811eff0e9004
mcurrie2 <81950635+mcurrie2@users.noreply.github.com>
2024-07-27 17:02:07 -0600

    Update 3DMixtureModel.py

--- 3DMixtureModel.py
+++ 3DMixtureModel.py
@@ -176,7 +176,7 @@ def fit_mixture_model(rvals, rhovals, covmats, base_path, out_dir=None, rmis_sam
             Mixture model parameters in the form 
             [log(alpha), log(beta), log(rho_s), log(r_s), log(r_t), f_mis, sigma_r]
         rvals: Nbins*1 array
-    	    Radial values (bin midpoints in R200m) of the input profiles
+    	    Radial bin midpoints (in R200m) of the input profiles
         rhovals: Nhalos*Nbins array
 	    Radial surface number densities (in (Mpc/h)^-3) of the input profiles
         covmats: Nhalos*Nbins*Nbins array
@@ -207,9 +207,8 @@ def fit_mixture_model(rvals, rhovals, covmats, base_path, out_dir=None, rmis_sam
         # ****************************
         overall_log_likelihood = 0
 
-        # Compute the theory predictions for both models.
-        r_data = rvals[0]            
-        rho_thr_D22 = rho_D22(theta, r_data) 
+        # Compute the theory predictions for both models.          
+        rho_thr_D22 = rho_D22(theta, rvals) 
 
         def prob_r_mis(r_mis):
             return r_mis/(sigma_r)**2 * np.exp(-(r_mis)**2 /(2*sigma_r**2))           
@@ -238,7 +237,7 @@ def fit_mixture_model(rvals, rhovals, covmats, base_path, out_dir=None, rmis_sam
         # Prob(data_i | mis-centered, theta)
         ##########################################
         def integrand(r_mis):                                       
-            diff_from_misc = rhovals - rho_mis_given_r_mis(theta, r_data, r_mis, phi_samples=phi_samples)
+            diff_from_misc = rhovals - rho_mis_given_r_mis(theta, rvals, r_mis, phi_samples=phi_samples)
 
             # Up next: gaussian_diff = np.exp(-1/2 * np.dot(diff_from_misc, np.linalg.solve(all_covmats, diff_from_misc)))
             partial = np.linalg.solve(covmats, diff_from_misc)
